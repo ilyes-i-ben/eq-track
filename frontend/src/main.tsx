f9582abbce24fcf1fbbb315c96e141b450000f1c
ilyes <ismailbenameur2004@gmail.com>
2025-07-07 00:21:00 +0200

    offline: setup apollo3-cache-persist package for caching

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,18 +3,30 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client'
+import { LocalStorageWrapper, persistCache } from 'apollo3-cache-persist'
 
 const API_URL = import.meta.env.VITE_API_URL as string
 
-const client = new ApolloClient({
-  uri: API_URL + '/graphql',
-  cache: new InMemoryCache(),
-})
+const cache = new InMemoryCache();
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <ApolloProvider client={client}>
-      <App />
-    </ApolloProvider>
-  </StrictMode>,
-)
+async function setupApolloCacheAndRender() {
+  await persistCache({
+    cache,
+    storage: new LocalStorageWrapper(window.localStorage)
+  });
+
+  const client = new ApolloClient({
+    uri: API_URL + '/graphql',
+    cache,
+  })
+
+  createRoot(document.getElementById('root')!).render(
+    <StrictMode>
+      <ApolloProvider client={client}>
+        <App />
+      </ApolloProvider>
+    </StrictMode>,
+  )
+}
+
+setupApolloCacheAndRender();
